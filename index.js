448a4c9d40a4a647254b1e7295c9aa480aa864ca
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2025-04-21 22:28:01 -0300

    create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const { chromium } = require('playwright')
+
+;(async () => {
+  const browser = await chromium.launch({ headless: false })
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  await page.goto('https://example.com')
+  await page.screenshot({ path: 'example.png' })
+  await browser.close()
+})()
